49e4f9aa9a3c4e8d404af9fbc7d9cb3cf21fb61e
hollie.durousseau <7085808+Sneling@users.noreply.github.com>
2024-07-15 09:00:21 +0100

    Add initial test cases for daily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 import os
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -29,6 +30,40 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """Test that the max function works on an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 4],
+                           [3, 2],
+                           [0, 1]])
+    test_result = np.array([3, 4])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_negatives():
+    """Test that the max function works with negative integers."""
+
+    from inflammation.models import daily_max
+
+    test_input = np.array([[-10, 4],
+                           [3, -3],
+                           [0, 1]])
+    test_result = np.array([3, 4])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)    
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
